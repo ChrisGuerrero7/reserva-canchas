27e2c1be24f005ead4738fd3ca0b9949a495b66c
Christhofer Guerrero Grillo <christhoferguerrerogrillo@MacBook-Air-de-Christhofer.local>
2025-06-07 13:22:44 -0500

    actualizacion de backend para que funcione el POST

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,19 @@ from routers import buscador
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+# Lista de orígenes permitidos (agrega tu frontend aquí)
+origins = [
+    "https://frontend-web-9tyd.onrender.com",  # reemplaza con la URL real
+    "http://localhost:5500",             # útil para pruebas locales
+]
+
 # Habilitar CORS para frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,            # Orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],              # Métodos permitidos (GET, POST, etc.)
+    allow_headers=["*"],              # Headers permitidos
 )
 
 # Incluir rutas
